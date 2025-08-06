2917341b2004deb23b94c705fe548de323432fc1
성현 <xpfl7920@gmail.com>
2025-08-06 23:50:41 +0900

    이미지 수정

--- src/page/AdminProductPage/component/NewItemDialog.js
+++ src/page/AdminProductPage/component/NewItemDialog.js
@@ -43,7 +43,6 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
     if (showDialog) {
       if (mode === "edit") {
         setFormData(selectedProduct);
-        // 객체형태로 온 stock을  다시 배열로 세팅해주기
         const sizeArray = Object.keys(selectedProduct.stock).map((size) => [
           size,
           selectedProduct.stock[size],
@@ -57,30 +56,24 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
   }, [showDialog]);
 
   const handleClose = () => {
-    //모든걸 초기화시키고
     setFormData({ ...InitialFormData });
     setStock([]);
     setStockError(false);
     dispatch(clearError());
-    // 다이얼로그 닫아주기
     setShowDialog(false);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    //재고를 입력했는지 확인, 아니면 에러
     if (stock.length === 0) return setStockError(true);
 
-    // 재고를 배열에서 객체로 바꿔주기
     const totalStock = stock.reduce((total, item) => {
       return { ...total, [item[0]]: parseInt(item[1]) };
     }, {});
 
     const size = stock.map((item) => item[0]);
 
-    // [['M',2]] 에서 {M:2}로
     if (mode === "new") {
-      //새 상품 만들기
       dispatch(createProduct({ ...formData, stock: totalStock, size: size }));
     } else {
       // 상품 수정하기
@@ -88,38 +81,32 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
   };
 
   const handleChange = (event) => {
-    //form에 데이터 넣어주기
     const { id, value } = event.target;
     setFormData({ ...formData, [id]: value });
   };
 
   const addStock = () => {
-    //재고타입 추가시 배열에 새 배열 추가
     setStock([...stock, ["", ""]]);
   };
 
   const deleteStock = (idx) => {
-    //재고 삭제하기
     const newStock = stock.filter((item, index) => index !== idx);
     setStock(newStock);
   };
 
   const handleSizeChange = (value, index) => {
-    //  재고 사이즈 변환하기
     const newStock = [...stock];
     newStock[index][0] = value;
     setStock(newStock);
   };
 
   const handleStockChange = (value, index) => {
-    //재고 수량 변환하기
     const newStock = [...stock];
     newStock[index][1] = value;
     setStock(newStock);
   };
 
   const onHandleCategory = (event) => {
-    // 카테고리 추가되어 있으면 제거
     if (formData.category.includes(event.target.value)) {
       const newCategory = formData.category.filter(
         (item) => item !== event.target.value
@@ -129,7 +116,6 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
         category: [...newCategory],
       });
     } else {
-      // 없으면 추가?
       setFormData({
         ...formData,
         category: [...formData.category, event.target.value],
@@ -138,7 +124,6 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
   };
 
   const uploadImage = (url) => {
-    //이미지 업로드
     setFormData({ ...formData, image: url });
   };
 
@@ -168,7 +153,6 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
               value={formData.sku}
             />
           </Form.Group>
-
           <Form.Group as={Col} controlId="name">
             <Form.Label>Name</Form.Label>
             <Form.Control
@@ -180,7 +164,6 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
             />
           </Form.Group>
         </Row>
-
         <Form.Group className="mb-3" controlId="description">
           <Form.Label>Description</Form.Label>
           <Form.Control
@@ -193,7 +176,6 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
             required
           />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="stock">
           <Form.Label className="mr-1">Stock</Form.Label>
           {stockError && (
@@ -216,18 +198,20 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
                     <option value="" disabled selected hidden>
                       Please Choose...
                     </option>
-                    {SIZE.map((sizeItem, sizeIndex) => (
+                    {/* ===== 원래 코드로 복원된 부분 ===== */}
+                    {SIZE.map((item, index) => (
                       <option
                         inValid={true}
-                        value={sizeItem.toLowerCase()}
+                        value={item.toLowerCase()}
                         disabled={stock.some(
-                          (size) => size[0] === sizeItem.toLowerCase()
+                          (size) => size[0] === item.toLowerCase()
                         )}
-                        key={sizeIndex}
+                        key={index}
                       >
-                        {sizeItem}
+                        {item}
                       </option>
                     ))}
+                    {/* ================================= */}
                   </Form.Select>
                 </Col>
                 <Col sm={6}>
@@ -254,11 +238,9 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
             ))}
           </div>
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="Image" required>
           <Form.Label>Image</Form.Label>
           <CloudinaryUploadWidget uploadImage={uploadImage} />
-
           {formData.image && (
             <img
               id="uploadedimage"
@@ -268,7 +250,6 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
             />
           )}
         </Form.Group>
-
         <Row className="mb-3">
           <Form.Group as={Col} controlId="price">
             <Form.Label>Price</Form.Label>
@@ -280,7 +261,6 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
               placeholder="0"
             />
           </Form.Group>
-
           <Form.Group as={Col} controlId="category">
             <Form.Label>Category</Form.Label>
             <Form.Control
@@ -297,7 +277,6 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
               ))}
             </Form.Control>
           </Form.Group>
-
           <Form.Group as={Col} controlId="status">
             <Form.Label>Status</Form.Label>
             <Form.Select
